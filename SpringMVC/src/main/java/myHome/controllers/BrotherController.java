6f25978e706452d75e15aecfec72042093dcc2c5
niyamat <niyamatbarote@gmail.com>
2026-01-17 23:47:23 +0530

    Used @Controller at the Class Level and Cerated multiple methods in same class

--- SpringMVC/src/main/java/myHome/controllers/BrotherController.java
+++ SpringMVC/src/main/java/myHome/controllers/BrotherController.java
@@ -4,13 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+//We Can Also Use the @ResponseBody on the Class Level
+// To Visit the bat page we need to use the destination as :- http://localhost:8082/SpringMVC/myHome.com/boy/bat
 @Controller
+@RequestMapping("/boy")
 public class BrotherController {
 	
 	@ResponseBody
 	@RequestMapping("/bat")
 	public String giveBat() {
 		return "Here is Cricket Bat For YOu ..!!";
+	}
+	
+//	We can Make many methods in same class with different urls with different actions --
+	
+	@ResponseBody
+	@RequestMapping("/book")
+	public String book() {
+		return "Just Take My Book and Don't Return..!!"	;
 	}
 
 }
