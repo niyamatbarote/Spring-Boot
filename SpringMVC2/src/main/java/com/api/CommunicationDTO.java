0811d6d7ecaa83057f5f9143275fb23dd09da93b
niyamat <niyamatbarote@gmail.com>
2026-02-20 23:12:15 +0530

    Dto prepared

--- SpringMVC2/src/main/java/com/api/CommunicationDTO.java
+++ SpringMVC2/src/main/java/com/api/CommunicationDTO.java
@@ -1,5 +1,16 @@
 package com.api;
 
 public class CommunicationDTO {
+	
+	private String email;
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
